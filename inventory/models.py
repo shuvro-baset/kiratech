3393c1cdaeed268f21b8cb872290420c2fc504ed
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-15 23:07:33 +0600

    model created

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Inventory(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=60)
+    note = models.TextField()
+    stock = models.IntegerField()
+    availability = models.BooleanField(default=False)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
